7523d60af3ab9b95f0953abb4ae97ef22ca606c2
Whitney <japanrules0423@gmail.com>
2023-01-29 00:12:48 -0600

    second commit

--- wargame.js
+++ wargame.js
@@ -1,60 +1,46 @@
 class Card {
-    constructor (suits, faces, values) {
-        this.suits = suits;
-        this.faces = faces;
-        this.values = values;
+    constructor (suit, face, value) {
+        this.suit = suit;
+        this.face = face;
+        this.value = value;
     }
-
 }
 
 class Deck {
     constructor () {
         this.cards = [];
     }
-        //create deck
-        createDeck() {
-            let suits = ['diamonds', 'hearts', 'spades', 'clubs'];
-            let faces = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K'];
-            let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-            for (let i = 0; i < suits.length; i++) {
-                for (let x = 0; x < faces.length; x++) {
-                    this.cards.push(new Card(suits[i], faces[x], values[x]));
-                }
+    createDeck() {
+        let suits = ['diamonds', 'hearts', 'spades', 'clubs'];
+        let faces = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10,'J', 'Q', 'K'];
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+        //add cards to this.cards array
+    
+        for (let i = 0; i < suits.length; i++) {
+            for (let x = 0; x < faces.length; x++) {
+                this.cards.push(new Card(suits[i], faces[x], values[x]));
             }
         }
-}
+        //for (let suit in this.suits) {
+        //    console.log( "index:", suit, "This is the card suit:",
+        //    this.suits[suit]);
 
-class Game {
-    constructor () {
-        this.cardsInMiddle = [];
-        this.players = [];
+        //    for (let face in this.faces) {
+        //       console.log( "index:", face, "This is the card face:",
+        //        this.faces[face]);
+
+        //        cardDeck.push({
+        //            value: this.values[face],
+        //            faceValue: this.faces[face] + " of " + this.suits[suit],
+        //       });
+        //    }
     }
-        //let deck = new Deck();
-        //deck.createDeck();
 
-        //shuffle deck
-        shuffleDeck() {
-            //for 1000 turns
-            //switch the values of two random cards
-            for (let i = 0; i < 1000; i++) {
-                let location1 = Math.floor ((Math.random()
-                    * this.cards.length));
-                let location2 = Math.floor ((Math.random()
-                    * this.cards.length));
-                tmp = this.cards[location1];
-                this.cards[location1] = this.cards[location2];
-                this.cards[location2] = tmp;
-            }
-        }
-    start(playerOneName, playerTwoName) {
-        this.players.push(new Player(playerOneName));
-        this.players.push(new Player(playerTwoName));
-        let deck = new Deck();
-        deck.createDeck();
-        deck.shuffleDeck();
-        this.players[0].playerCards = deck.cards.slice(0, 26);
-        this.players[1].playerCards =  deck.cards.slice(26, 52);
+    //shuffle deck
+    shuffle() {
+        this.cards.sort(() => (Math.random() > 0.5) ? 1: -1);
     }
 }
 
@@ -62,11 +48,75 @@ class Player {
     constructor (name) {
         this.playerName = name;
         this.playerCards = [];
+        this.points = 0;
     }
 
 }
 
-let Game = new Game();
-Game.start('Player 1', 'Player 2');
+class Game {
+    constructor () {
+        this.cards = [];
+        this.playerCards = [];
+        this.player1 = new Player('Player One');
+        this.player2 = new Player('Player Two');
+        this.points = 0;
+    }
 
-console.log(Game.players);
+    createDeck() {
+        let suits = ['diamonds', 'hearts', 'spades', 'clubs'];
+        let faces = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10,'J', 'Q', 'K'];
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+        //add cards to this.cards array
+    
+        for (let i = 0; i < suits.length; i++) {
+            for (let x = 0; x < faces.length; x++) {
+                this.cards.push(new Card(suits[i], faces[x], values[x]));
+            }
+        }
+    }
+}
+
+let game = new Game();
+
+game.createDeck();
+
+let deck = game.cards
+for (let i = 0; i < 52; i++) {
+    let tempCard = deck[i];
+    let randomIndex = Math.floor(Math.random() * 52);
+    deck[i] = deck [randomIndex];
+    deck [randomIndex] = tempCard;
+}
+//split deck into player decks
+this.player1.playerCards = deck.slice(0, 26);
+this.player2.playerCards = deck.slice(26, 52);
+
+for(let i = 0; i < 26; i++) {
+    let winner;
+    let round = i + 1;
+    console.log(`Player One: ${this.player1.playerCards[i].name} vs Player Two: ${this.player2.playerCards[i].name}`);
+       
+    if (this.player1.playerCards[i].value > this.player2.playerCards[i].value){
+        this.player1.points++;
+        console.log('Player One wins the round!');
+    } else if (this.player1.playerCards[i].value < this.player2.playerCards[i].value){
+        this.player2.points++;
+        console.log('Player Two wins the round!');
+    } else {
+        console.log(`Tie: No points awarded.`);
+    }
+
+    console.log(`Player One: ${this.player1.points}, Player Two: ${this.player2.points}`);
+
+    if (this.player1.points > this.player2.points) {
+        winner = this.player1;
+    } else if (this.player1.points < this.player2.points) {
+        winner = this.player2;
+    } else {
+        console.log('Tied Game');
+    }
+    
+    this.end(winner);
+    return winner;
+}
